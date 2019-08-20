3199f80d0e413ee8a6e27b8ab219e06ee3db1232
Jerry Wang <jerryappleid761208@gmail.com>
2019-08-20 11:53:05 -0700

    deposit action

--- src/actions/balance.test.js
+++ src/actions/balance.test.js
@@ -8,3 +8,11 @@ it('creates an action to set the balance', () => {
 
   expect(actions.setBalance(balance)).toEqual(expectedAction);
 });
+
+it('creates an action to deposit into the balance', () => {
+  const deposit = 10;
+
+  const expectedAction = { type: constants.DEPOSIT, deposit };
+
+  expect(actions.deposit(deposit)).toEqual(expectedAction);
+})
