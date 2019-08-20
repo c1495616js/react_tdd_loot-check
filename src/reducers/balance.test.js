5dcd7575006355cd40de61db27ae13ed3862b92d
Jerry Wang <jerryappleid761208@gmail.com>
2019-08-20 13:06:27 -0700

    test for reducer deposit

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -4,9 +4,17 @@ import * as constants from '../actions/constants';
 describe('balanceReducer', () => {
   it('sets a balance', () => {
     const balance = 10;
-        
+
     expect(
       balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
     ).toEqual(balance);
+  });
+
+  it('deposits into the balance', () => {
+    const deposit = 10;
+    const initialState = 5;
+
+    expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit }))
+      .toEqual(initialState + deposit);
   })
 })
