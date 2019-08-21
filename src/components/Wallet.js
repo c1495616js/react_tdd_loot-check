d662402ef9975250a5323155cb33f96288bd6260
Jerry Wang <jerryappleid761208@gmail.com>
2019-08-21 09:23:18 -0700

    small fix

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -15,7 +15,7 @@ export class Wallet extends Component {
   render() {
     return (
       <div>
-        <h3 className="balance">Wallet balance: {this.state.balance}</h3>
+        <h3 className="balance">Wallet balance: {this.props.balance}</h3>
         <br />
         <input 
           className="input-wallet"
