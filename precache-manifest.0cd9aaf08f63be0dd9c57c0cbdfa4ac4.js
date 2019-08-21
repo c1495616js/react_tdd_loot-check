6a65aaf8137b0b726e5ddf638bdd1d62f0e8edea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-21 17:40:44 +0000

    Deploy c1495616js/react_tdd_loot-check to github.com/c1495616js/react_tdd_loot-check.git:gh-pages

---
+++ precache-manifest.0cd9aaf08f63be0dd9c57c0cbdfa4ac4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e970dcd410548e4c2753896599502e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9b1ed9853a82a6ea31f",
+    "url": "/static/js/2.6713ef60.chunk.js"
+  },
+  {
+    "revision": "330f18f19d91e045310d",
+    "url": "/static/js/main.9e56a653.chunk.js"
+  },
+  {
+    "revision": "0a0361758869347fc4d9",
+    "url": "/static/js/runtime~main.9289ccc8.js"
+  }
+]);
